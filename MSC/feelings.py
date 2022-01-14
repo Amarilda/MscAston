c3496e23b4094f66ef10da029ef5ff627a23295b
Amarilda <Amarilda@users.noreply.github.com>
2022-01-14 17:44:21 +0100

    Update feelings.py

--- MSC/feelings.py
+++ MSC/feelings.py
@@ -1,26 +1,21 @@
 import pandas as pd
 import datetime
 from docxlatex import Document
+import shutil
 
 def ThinkingTheFeelings():
     df = pd.read_csv("/Users/Edite/Documents/GitHub/KPI/feelings.csv")
     if pd.to_datetime(df.date[len(df)-1]) != (datetime.datetime.now()).date():
         answer = []
-
-        #datums = (datetime.datetime.now() - datetime.timedelta(days=1)).date()
         datums = (datetime.datetime.now()).date()
         print(datums.strftime('%d/%m/%y'))
-        answer.append(datums)        
-            
+        answer.append(datums)                   
         for i in df.columns[1:]:
             print(i)
             i = input() or 0
-            answer.append(i)
-            
+            answer.append(i)           
         df.loc[len(df)] = answer
         df.to_csv("/Users/Edite/Documents/GitHub/KPI/feelings.csv", index = False) 
-        print("One day closer to year in pixels")
-        print()
     else:
         print('You got this')
         pass
@@ -29,19 +24,22 @@ def ThinkingTheFeelings():
     df = pd.read_csv('MSC/amarilda.csv')
     atbilde = []
     atbilde.append(datetime.datetime.now())
-
     doc = Document("/Users/Edite/Desktop/Amarilda.docx")
     text = doc.get_text()
     atbilde.append(len(text.split()))
-
     df.loc[len(df)] = atbilde
     df.to_csv('MSC/amarilda.csv', index = False)
+
+    maximums = max(df['word count'])
+    if (datetime.datetime.now()- pd.to_datetime(min(df.entry_date[df['word count'] == int(maximums) ]))).days >= 3:
+        print('Please top up your Path Authoring')
+
 
     if df['word count'].iloc[-1] == df['word count'].iloc[-2]:
         pass
     else:
         print('Self authoring. Words written: ', df['word count'].iloc[-1] - df['word count'].iloc[-2])
-        print('short: '+ str(round(df['word count'].iloc[-1]/(7*6*1000),2)*100) +'% done, long: ' +str(round(df['word count'].iloc[-1]/(7*6*3000),2)*100) +'% done')
+        print('short: '+ str(round(df['word count'].iloc[-1]/(7*6*1000),3)*100) +'% done, long: ' +str(round(df['word count'].iloc[-1]/(7*6*3000),3)*100) +'% done')
 
         ## read the doc    
         doc = Document("/Users/Edite/Desktop/Amarilda.docx")
@@ -74,12 +72,13 @@ def ThinkingTheFeelings():
         for i in range(0,len(df)):
             word_count.append(len(bodies[df.start[i]:df.end[i]].split())-len(df.heading[i].split()))
         df['word count'] = word_count
-
         df['date'] = datetime.date.today()
-
         df = df[['date','chapter','heading', 'word count']]
         main.append(df).to_csv('MSC/amarilda_hist.csv', index = False)
 
-    if df[-1:].index [0] -df.index[df.entry_date == df.entry_date[df['word count'] ==max(df['word count'])].min()] [0] >= 3:
-        print('Please top up your Path Authoring')
-
+        #create a cony of file for future NLP
+        os.chdir('/Users/Edite/Desktop')
+        renFolder= '/Volumes/Seagate Backup Plus Drive/Learning/Past Authoring'
+        oldname = 'Amarilda.docx'
+        newname= 'Amarilda_'+datetime.date.today().strftime('%Y_%m_%d') +'.docx'
+        shutil.copy(oldname, renFolder+'/'+newname)
